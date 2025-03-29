ec32c4e6faeb4cdfd7bcb7f91eb4493fc001dbe8
Dhruv-Varshney-developer <dhruvvarshney77@gmail.com>
2025-03-29 10:44:48 +0530

    chore: update packages and hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,12 +1,12 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import "hardhat-deploy";
 
-require("dotenv").config();
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.19",
+    version: "0.8.26",
     settings: {
       optimizer: {
         enabled: true,
